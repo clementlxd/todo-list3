505f5921d6c38cd0c7850a5c60e962087d423584
lxd <clementlxd@139.com>
2017-09-24 11:12:30 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
+import 'normalize.css'
+import './reset.css'
 
 class App extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            newTodo:'test',
+            todoList:[
+                {id:1,title:'第一个待办'},
+                {id:2,title:'第二个待办'}
+            ]
+        }
+    }
   render() {
+        let todos=this.state.todoList.map((item,index)=>{
+            return(
+                <li key={index}>
+                    <TodoItem todo={item} onToggle={this.toggle.bind(this)}
+                      onDelete={this.delete.bind(this)}/>
+                </li>
+            )
+        })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1>我的待办</h1>
+          <div className='inputWrapper'>
+              <TodoInput content={this.state.newTodo}
+                onChange={this.changeTitle.bind(this)}
+                onSubmit={this.addTodo.bind(this)}/>
+          </div>
+          <ol>
+              {todos}
+          </ol>
       </div>
     );
   }
+
+  toggle(e,todo){
+      todo.status=todo.status==='completed'?'':'completed'
+      this.setState(this.state)
+  }
+
+  changTitle(event){
+        this.setState({
+            newTodo:event.target.value,
+            todoList:this.state.todoList
+        })
+  }
+
+
+  addTodo(){
+        console.log('我得添加一个todo了')
+  }
+
+  delete(event,todo){
+      todo.deleted=true
+      this.setState(this.state)
+  }
 }
+
+
 
 export default App;
